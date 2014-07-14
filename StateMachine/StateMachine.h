8405750bc87d1a4d4e5d9b2de50fb43027b16232
Local Administrator <localadm@SCI-XXXX.local>
2014-07-14 23:13:30 +1000

    Initial Commit

---
+++ StateMachine/StateMachine.h
@@ -0,0 +1,19 @@
+//
+//  StateMachine.h
+//  StateMachine
+//
+//  Created by Local Administrator on 14/07/2014.
+//  Copyright (c) 2014 Local Administrator. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for StateMachine.
+FOUNDATION_EXPORT double StateMachineVersionNumber;
+
+//! Project version string for StateMachine.
+FOUNDATION_EXPORT const unsigned char StateMachineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StateMachine/PublicHeader.h>
+
+
